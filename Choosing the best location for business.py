a9ce6ce343a104f54c857ebf38598adee9c88d4f
fabzfta <fabriciotdalmeida@gmail.com>
2022-03-28 18:23:53 -0300

    Choosing the best location for business

---
+++ Choosing the best location for business.py
@@ -0,0 +1,17 @@
+#You need a location with a great deal of space and relatively few businesses and people around to avoid complaints about the smell.
+# 3 Databases: The land_use table has info on the percentage of vacant land by city ward. The census table has population by ward, and the licenses table lists businesses by ward.
+
+# Merging land_use and census and merge resulting with licenses including suffixes
+land_cen_lic = land_use.merge(census, on='ward') \
+                    .merge(licenses, on='ward', suffixes=('_cen','_lic'))
+
+# Grouping by ward, pop_2010, and vacant, then counting the # of accounts
+pop_vac_lic = land_cen_lic.groupby(['ward','pop_2010','vacant'],
+                                   as_index=False).agg({'account':'count'})
+
+# Sorting pop_vac_lic and printing the results
+sorted_pop_vac_lic = pop_vac_lic.sort_values(["vacant","account","pop_2010"],
+                                             ascending=[False,True,True])
+
+# Printing the top few rows of sorted_pop_vac_lic
+print(sorted_pop_vac_lic.head())
