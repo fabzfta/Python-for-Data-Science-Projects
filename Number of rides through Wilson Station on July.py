b8b6574e4002b5b287683e4887a6e7720aabe453
fabzfta <fabriciotdalmeida@gmail.com>
2022-03-28 17:46:28 -0300

    Number of rides through Wilson Station on July

---
+++ Number of rides through Wilson Station on July.py
@@ -0,0 +1,16 @@
+#In this exercise we have to find the number os passangers that traveled by Chicago's public transportation through Willson station during all weekdays on July.
+#We use three different databases: cal, ridership and stations DataFrames
+
+# Merging the ridership, cal, and stations tables
+ridership_cal_stations = ridership.merge(cal, on=['year','month','day']) \
+							.merge(stations, on='station_id')
+
+# Creating a filter to filter ridership_cal_stations
+filter_criteria = ((ridership_cal_stations['month'] == 7)
+                   & (ridership_cal_stations['day_type'] == "Weekday")
+                   & (ridership_cal_stations['station_name'] == "Wilson"))
+
+# Using .loc and the filter to select for rides
+print(ridership_cal_stations.loc[filter_criteria, 'rides'].sum())
+
+# 140005 Passengers.
