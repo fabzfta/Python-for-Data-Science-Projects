fed94aa721718c70c54b5f4181dd3f928aa8ec85
fabzfta <fabriciotdalmeida@gmail.com>
2022-03-28 23:09:51 -0300

    Concatenating Invoices to study total avg monthly inv.py

---
+++ Concatenating Invoices to study total avg monthly inv.py
@@ -0,0 +1,16 @@
+#You have been given three tables with invoice data named inv_jul, inv_aug, and inv_sep. Concatenate these tables into one to create a graph of the average monthly invoice total.
+
+
+# Concatenating the tables and adding keys
+inv_jul_thr_sep = pd.concat([inv_jul, inv_aug, inv_sep],
+                            keys=['7Jul','8Aug','9Sep'])
+
+# Grouping the invoices by the index keys and finding avg of the total column
+avg_inv_by_month = inv_jul_thr_sep.groupby(level=0).agg({'total':'mean'})
+
+# Bar plot of avg_inv_by_month
+avg_inv_by_month.plot(kind='bar')
+plt.show()
+
+
+#The plot shows that September had the highest average invoice total.
